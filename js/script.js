4e51c369697c1b6e5dd763ca10785715b5249323
donia <doniaafify615@gmail.com>
2025-08-18 20:15:05 +0300

    files uploaded.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const dark=document.querySelector(".menu-bg");
+const img =document.querySelector('.mockup')
+const menu=document.getElementById('mainNav');
+menu.addEventListener('show.bs.collapse',()=>{
+    dark.classList.add('dark-bg');
+    img.style.opacity = '0'
+});
+menu.addEventListener('hide.bs.collapse', ()=>{
+    dark.classList.remove('dark-bg');
+});
